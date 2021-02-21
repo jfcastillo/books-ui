584e06c792a7e2c27d2b9a82a8f307d724062b9a
Felipe Castillo <jfcastillo1204@gmail.com>
2021-02-21 01:18:22 -0500

    cors added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 app.use(express.static('./dist/books-ui'));
 app.get('/*', function (req, res) {
     res.sendFile('index.html', { root: 'dist/books-ui/' }
